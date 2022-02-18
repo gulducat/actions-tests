6114d0c426b977e5721bf59d433427bd02eb750a
Daniel Bennett <dbennett@hashicorp.com>
2022-02-18 15:09:15 -0600

    use my new go-run-programs lib

--- main_functional_test.go
+++ main_functional_test.go
@@ -3,97 +3,29 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"os/exec"
 	"testing"
-	"time"
+
+	gorun "github.com/gulducat/go-run-programs/hcl"
 )
 
-type testProgram struct {
-	Name  string
-	Run   []string
-	Check []string
-	Test  func(*testing.T, string)
-}
-
-func (tp testProgram) String() string {
-	return tp.Name
-}
-
 func TestFunctional(t *testing.T) {
-	t.Setenv("VAULT_ADDR", "http://127.0.0.1:8200") // default is https, and -dev doesn't do tls
-
-	// programs to run for us to test against
-	programs := []testProgram{
-		{
-			Name:  "consul",
-			Run:   []string{"consul", "agent", "-dev"},
-			Check: []string{"consul", "members"},
-			Test:  pretendToTest, // testConsul
-		},
-		{
-			Name:  "nomad",
-			Run:   []string{"nomad", "agent", "-dev"},
-			Check: []string{"nomad", "node", "status"},
-			Test:  pretendToTest, // testNomad
-		},
-		{
-			Name:  "vault",
-			Run:   []string{"vault", "server", "-dev"},
-			Check: []string{"vault", "status"},
-			Test:  pretendToTest, // testVault
-		},
-	}
-	runPrograms(t, programs...)
-
-	t.Run("all", func(t *testing.T) {
-		out := runBinary(t, "all")
-		pretendToTest(t, out)
-	})
-
-	for _, p := range programs {
-		t.Run(p.Name, func(t *testing.T) {
-			out := runBinary(t, p.Name)
-			p.Test(t, out)
-		})
-	}
-}
-
-func runWithContext(ctx context.Context, stop context.CancelFunc, command ...string) {
-	out, err := exec.CommandContext(ctx, command[0], command[1:]...).CombinedOutput()
+	// start programs
+	stop, err := gorun.RunFromHCL("programs.hcl")
+	defer stop()
 	if err != nil {
-		fmt.Printf("XXXXX err running %s:\n  XXXoutput: %s\n  XXXerror: %s\nXXXXX\n", command, out, err)
-		stop() // stop everything now
-	}
-}
-
-func runPrograms(t *testing.T, programs ...testProgram) {
-	t.Log("starting:", programs)
-
-	// run programs in the background
-	ctx, stop := context.WithCancel(context.Background())
-	t.Cleanup(stop) // stop them after test is done
-	for _, p := range programs {
-		go runWithContext(ctx, stop, p.Run...)
+		t.Fatal(err)
 	}
 
-	// wait for programs to start
-	var (
-		bts []byte
-		err error
-	)
-	for _, p := range programs {
-		for x := 0; x < 20; x++ { // TODO: 20 seconds max wait time?
-			bts, err = exec.Command(p.Check[0], p.Check[1:]...).CombinedOutput()
-			if err == nil {
-				break
-			}
-			time.Sleep(time.Second)
-		}
-		if err != nil {
-			t.Fatalf("error during test setup: '%s' error: %s: %s", p.Check, err, string(bts))
-		}
+	args := []string{
+		"all", "consul", "nomad", "vault",
+	}
+
+	for _, a := range args {
+		t.Run(a, func(t *testing.T) {
+			out := runBinary(t, a)
+			pretendToTest(t, out)
+		})
 	}
 }
 
---
+++ programs.hcl
@@ -0,0 +1,18 @@
+program "consul" {
+  command = "consul agent -dev"
+  check   = "consul members"
+}
+  
+program "nomad" {
+	command = "nomad agent -dev"
+	check   = "nomad node status"
+}
+  
+program "vault" {
+  command = "vault server -dev"
+  check   = "vault status"
+  env = {
+    # default client is https, but vault server -dev is http
+    VAULT_ADDR = "http://localhost:8200"
+  }
+}
