3a33808f88ef06a22ec3749c9302fb1ef82089a4
Daniel Bennett <daniel.bennett1@ibm.com>
2025-11-07 16:58:05 -0500

    add dispatch runner script

---
+++ run-and-watch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function run() {
+  local workflow uid
+  workflow="$1" ; shift
+  uid="$1" ; shift
+  echo "workflow: $workflow"
+  echo "unique id: '$uid'"
+  echo "run args: $@"
+
+  gh workflow run "$workflow" "$@"
+  
+  echo 'wait for run to start'
+  for _ in {1..20}; do
+    gh run list --workflow "$workflow" \
+      --json databaseId,displayTitle,status,url \
+      --jq ".[] | select(.displayTitle | contains(\"$uid\"))" \
+      | tee run.json | grep . && break
+    sleep 1
+    printf .
+  done
+  
+  echo 'watch the run'
+  gh run watch --interval 3 --exit-status $(jq -r '.databaseId' < run.json)
+}
+
+run "$@"
+
